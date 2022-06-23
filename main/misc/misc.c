1b8cd0d0672c827dabde2d5bd7c20d3371ebb8e4
Vladimir Pinchuk <vladimir.pinchuk01@gmail.com>
2022-06-23 16:19:47 +0300

    add led

---
+++ main/misc/misc.c
@@ -0,0 +1,49 @@
+#include "misc.h"
+
+#include "global_context.h"
+
+#include <driver/gpio.h>
+#include <driver/sigmadelta.h>
+
+#include <freertos/task.h>
+
+#define LED_GPIO 5
+
+void led_task(void* params) {
+    sigmadelta_config_t sigmadelta_cfg = {
+        .channel = SIGMADELTA_CHANNEL_0,
+        .sigmadelta_prescale = 80,
+        .sigmadelta_duty = 0,
+        .sigmadelta_gpio = LED_GPIO,
+    };
+    sigmadelta_config(&sigmadelta_cfg);
+
+    int duty = 0;
+    bool dir = false;
+
+    while (true) {
+        if (gctx.logger_control.busy) {
+            sigmadelta_set_duty(SIGMADELTA_CHANNEL_0, 127);
+            vTaskDelay(100 / portTICK_PERIOD_MS);
+            sigmadelta_set_duty(SIGMADELTA_CHANNEL_0, -128);
+            vTaskDelay(100 / portTICK_PERIOD_MS);
+            sigmadelta_set_duty(SIGMADELTA_CHANNEL_0, 127);
+            vTaskDelay(100 / portTICK_PERIOD_MS);
+            sigmadelta_set_duty(SIGMADELTA_CHANNEL_0, -128);
+            vTaskDelay(800 / portTICK_PERIOD_MS);
+            duty = 0;
+        } else {
+            vTaskDelay(10 / portTICK_PERIOD_MS);
+            sigmadelta_set_duty(SIGMADELTA_CHANNEL_0, duty - 128);
+        duty_retry:
+            duty += dir ? -4 : 4;
+            if (duty > 255) {
+                dir = true;
+                goto duty_retry;
+            } else if (duty < 0) {
+                dir = false;
+                goto duty_retry;
+            }
+        }
+    }
+}
