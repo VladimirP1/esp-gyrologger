bf935d6a72ed11f85d353dd31dc7e9708a1089da
Vladimir Pinchuk <vladimir.pinchuk01@gmail.com>
2022-06-26 02:46:00 +0300

    cleanup

--- main/gyro/gyro_bmi160.cpp
+++ main/gyro/gyro_bmi160.cpp
@@ -132,10 +132,6 @@ static bool IRAM_ATTR gyro_timer_cb_c(void* args) { return gyro_timer_cb(args); 
 }
 
 void gyro_bmi160_task(void* params) {
-    // gctx.gyro_raw_to_rads = (1.0 / 32.8 * 3.141592 / 180.0);
-    // gctx.accel_raw_to_g = (16.0 / 32767);
-    // gctx.gyro_interp_interval = 600;
-    // gctx.gyro_decimate = 3;
     static uint8_t data[2];
     i2c_register_read(dev_adr, 0x00, data, 1);
     ESP_LOGI(TAG, "WHO_AM_I @ 0x%02X = 0x%02X", dev_adr, data[0]);
@@ -161,64 +157,6 @@ void gyro_bmi160_task(void* params) {
 
     i2c_register_write_byte(dev_adr, REG_FIFO_CONFIG_0, 0);
     i2c_register_write_byte(dev_adr, REG_FIFO_CONFIG_1, 0b11010000);
-
-    // int xx = 0;
-    // while (1) {
-    //     uint8_t tmp_data[16];
-    //     xx++;
-    //     if (xx % 500 == 0) {
-    //         // ESP_LOGI(TAG, "td0: 0x%02X", tmp_data[0]);
-    //         // i2c_register_read(dev_adr, 0x02, tmp_data, 1);
-    //         // ESP_LOGI(TAG, "err: 0x%02X", tmp_data[0]);
-    //         // i2c_register_read(dev_adr, 0x03, tmp_data, 1);
-    //         // ESP_LOGI(TAG, "pmu: 0x%02X", tmp_data[0]);
-    //         // i2c_register_read(dev_adr, REG_FIFO_CONFIG_1, tmp_data, 1);
-    //         // ESP_LOGI(TAG, "fif_cfg1: 0x%02X", tmp_data[0]);
-    //         // i2c_register_read(dev_adr, REG_GYR_CONF, tmp_data, 1);
-    //         // ESP_LOGI(TAG, "gyr_conf: 0x%02X", tmp_data[0]);
-    //         // i2c_register_read(dev_adr, REG_FIFO_LENGTH_0, tmp_data, 2);
-    //         const int fifo_bytes = tmp_data[0] | ((tmp_data[1] & 0x03) << 8);
-    //         ESP_LOGI(TAG, "fif_bytes: %d", fifo_bytes);
-    //         vTaskDelay(50);
-    //     }
-
-    //     i2c_register_read(dev_adr, REG_FIFO_DATA, tmp_data, 1);
-    //     uint8_t fh_mode = tmp_data[0] >> 6;
-    //     uint8_t fh_parm = (tmp_data[0] >> 2) & 0x0f;
-    //     if (tmp_data[0] == 0x80) {  // invalid / empty frame
-    //         i2c_register_read(dev_adr, REG_FIFO_DATA, tmp_data, 2);
-    //         vTaskDelay(1);
-    //         continue;
-    //     } else if (fh_mode == 2) {  // regular frame
-    //         int bytes_to_read = 1;
-    //         if (fh_parm & 1) {  // have acc
-    //             bytes_to_read += 6;
-    //         }
-    //         if (fh_parm & 2) {  // have gyr
-    //             bytes_to_read += 6;
-    //         }
-    //         if (fh_parm & 4) {  // have mag
-    //             bytes_to_read += 6;
-    //         }
-    //         i2c_register_read(dev_adr, REG_FIFO_DATA, tmp_data, bytes_to_read);
-    //     } else if (fh_mode == 1) {  // control frame
-    //         if (fh_parm == 0) {     // skip
-    //             i2c_register_read(dev_adr, REG_FIFO_DATA, tmp_data, 2);
-    //             ESP_LOGE(TAG, "skip %d", tmp_data[1]);
-    //             i2c_register_write_byte(dev_adr, REG_CMD, 0xb0);  // fifo reset
-
-    //         } else if (fh_parm == 1) {  // sensortime
-    //             // TODO
-    //             abort();
-    //         } else if (fh_parm == 2) {  // fifo input config
-    //             // TODO
-    //             abort();
-    //         } else {
-    //             ESP_LOGI(TAG, "unk: 0x%02X", tmp_data[0]);
-    //         }
-    //     }
-    //     // ESP_LOGI(TAG, "fifo: 0x%02X", tmp_data[0]);
-    // }
 
     timer_config_t config = {
         .alarm_en = TIMER_ALARM_EN,
