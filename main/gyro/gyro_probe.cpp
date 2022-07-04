f90b70242f902bb79d2e9611d23c7accb1b51acd
Vladimir Pinchuk <vladimir.pinchuk01@gmail.com>
2022-07-04 17:47:29 +0300

    try probing 3 times

--- main/gyro/gyro_probe.cpp
+++ main/gyro/gyro_probe.cpp
@@ -21,17 +21,20 @@ void gyro_probe_and_start_task() {
     static const std::initializer_list<probe_entry> probe_list = {
         // {0x68, probe_bmi160, gyro_bmi160_task},   {0x69, probe_bmi160, gyro_bmi160_task},
         // {0x6a, probe_lsm6, gyro_lsm6_task},       {0x6b, probe_lsm6, gyro_lsm6_task},
-        {0x68, probe_mpu6050, gyro_mpu6050_task}, {0x69, probe_mpu6050, gyro_mpu6050_task}};
+        {0x68, probe_mpu6050, gyro_mpu6050_task},
+        {0x69, probe_mpu6050, gyro_mpu6050_task}};
 
     for (auto& entry : probe_list) {
         mini_i2c_hw_fsm_reset();
-        // for (int i = 0; i < 3; ++i) {
-        if (entry.probe_ptr(entry.i2c_addr)) {
-            gctx.gyro_i2c_adr = entry.i2c_addr;
-            xTaskCreate(entry.task_ptr, "gyro-task", 4096, NULL, configMAX_PRIORITIES - 1, NULL);
-            return;
+        for (int i = 0; i < 3; ++i) {
+            if (entry.probe_ptr(entry.i2c_addr)) {
+                gctx.gyro_i2c_adr = entry.i2c_addr;
+                xTaskCreate(entry.task_ptr, "gyro-task", 4096, NULL, configMAX_PRIORITIES - 1,
+                            NULL);
+                return;
+            }
+            vTaskDelay(50 / portTICK_PERIOD_MS);
         }
-        // }
     }
     ESP_LOGE("gyro_probe", "No gyro found!");
 }
