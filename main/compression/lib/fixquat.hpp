583b756863815c249cbbf59a202057a9eb71ccd1
Vladimir Pinchuk <vladimir.pinchuk01@gmail.com>
2022-07-06 14:44:27 +0300

    change fixquat base type range

--- main/compression/lib/fixquat.hpp
+++ main/compression/lib/fixquat.hpp
@@ -9,7 +9,7 @@
 
 namespace quat {
 
-using base_type = fpm::fixed<std::int32_t, std::int64_t, 28>;
+using base_type = fpm::fixed<std::int32_t, std::int64_t, 27>;
 
 struct vec {
     vec() : x(0), y(0), z(0) {}
