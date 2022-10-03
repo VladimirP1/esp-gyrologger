eb381a9c08c2257a77501a350cbf2a6f5a16abd5
Vladimir Pinchuk <vladimir.pinchuk01@gmail.com>
2022-10-03 10:12:44 +0300

    gopro wifi trigger support

--- main/wifi/cam_control.cpp
+++ main/wifi/cam_control.cpp
@@ -18,69 +18,75 @@ extern "C" {
 
 #define TAG "camera_task"
 
-void firefly_x_lite_task(void* param) {
-    std::string rx_buffer;
-    char host_ip[] = "192.168.42.1";
+static bool make_tcp_conn(char* host, int port, int* sock_ret) {
     int addr_family = 0;
     int ip_protocol = 0;
 
+    struct sockaddr_in dest_addr;
+    dest_addr.sin_addr.s_addr = inet_addr(host);
+    dest_addr.sin_family = AF_INET;
+    dest_addr.sin_port = htons(port);
+    addr_family = AF_INET;
+    ip_protocol = IPPROTO_IP;
+
+    int sock = socket(addr_family, SOCK_STREAM, ip_protocol);
+    if (sock < 0) {
+        ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+        close(sock);
+        return false;
+    }
+
+    {
+        int flags = fcntl(sock, F_GETFL);
+        if (fcntl(sock, F_SETFL, flags | O_NONBLOCK) == -1) {
+            ESP_LOGE(TAG, "Unable to set socket non blocking");
+        }
+    }
+
+    ESP_LOGI(TAG, "Socket created, connecting to %s:%d", host, port);
+
+    int err = connect(sock, (struct sockaddr*)&dest_addr, sizeof(struct sockaddr_in6));
+    if (err != 0 && errno != EINPROGRESS) {
+        ESP_LOGE(TAG, "Socket unable to connect: errno %d", errno);
+        close(sock);
+        return false;
+    }
+
+    {
+        fd_set fdset;
+        FD_ZERO(&fdset);
+        FD_SET(sock, &fdset);
+        struct timeval tv = {3, 0};
+        int res = select(sock + 1, NULL, &fdset, NULL, &tv);
+        if (res <= 0) {
+            ESP_LOGE(TAG, "Socket unable to connect2: errno %d", errno);
+            close(sock);
+            return false;
+        }
+    }
+    ESP_LOGI(TAG, "Successfully connected");
+
+    {
+        int flags = fcntl(sock, F_GETFL);
+        if (fcntl(sock, F_SETFL, flags & ~O_NONBLOCK) == -1) {
+            ESP_LOGE(TAG, "Unable to set socket blocking");
+            close(sock);
+            return false;
+        }
+    }
+    *sock_ret = sock;
+    return true;
+}
+
+void firefly_x_lite_task(void* param) {
+    std::string rx_buffer;
     rx_buffer.resize(128);
-
+    int sock{}, err{};
     while (1) {
-        struct sockaddr_in dest_addr;
-        dest_addr.sin_addr.s_addr = inet_addr(host_ip);
-        dest_addr.sin_family = AF_INET;
-        dest_addr.sin_port = htons(7878);
-        addr_family = AF_INET;
-        ip_protocol = IPPROTO_IP;
-
-        int sock = socket(addr_family, SOCK_STREAM, ip_protocol);
-        if (sock < 0) {
-            ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
-            close(sock);
+        if (!make_tcp_conn("192.168.42.1", 7878, &sock)) {
             vTaskDelay(2000 / portTICK_PERIOD_MS);
             continue;
         }
-
-        {
-            int flags = fcntl(sock, F_GETFL);
-            if (fcntl(sock, F_SETFL, flags | O_NONBLOCK) == -1) {
-                ESP_LOGE(TAG, "Unable to set socket non blocking");
-            }
-        }
-
-        ESP_LOGI(TAG, "Socket created, connecting to %s:%d", host_ip, 7878);
-
-        int err = connect(sock, (struct sockaddr*)&dest_addr, sizeof(struct sockaddr_in6));
-        if (err != 0 && errno != EINPROGRESS) {
-            ESP_LOGE(TAG, "Socket unable to connect: errno %d", errno);
-            close(sock);
-            vTaskDelay(2000 / portTICK_PERIOD_MS);
-            continue;
-        }
-
-        {
-            fd_set fdset;
-            FD_ZERO(&fdset);
-            FD_SET(sock, &fdset);
-            struct timeval tv = {3, 0};
-            int res = select(sock + 1, NULL, &fdset, NULL, &tv);
-            if (res <= 0) {
-                ESP_LOGE(TAG, "Socket unable to connect2: errno %d", errno);
-                close(sock);
-                vTaskDelay(2000 / portTICK_PERIOD_MS);
-                continue;
-            }
-        }
-        ESP_LOGI(TAG, "Successfully connected");
-
-        {
-            int flags = fcntl(sock, F_GETFL);
-            if (fcntl(sock, F_SETFL, flags & ~O_NONBLOCK) == -1) {
-                ESP_LOGE(TAG, "Unable to set socket blocking");
-            }
-        }
-
         const char session_start[] = R"_({"msg_id":257,"token":0})_";
         err = send(sock, session_start, strlen(session_start), 0);
         if (err < 0) {
@@ -93,7 +99,7 @@ void firefly_x_lite_task(void* param) {
                 break;
             } else {
                 rx_buffer[len] = 0;
-                ESP_LOGI(TAG, "Received %d bytes from %s:", len, host_ip);
+                ESP_LOGI(TAG, "Received %d bytes");
                 ESP_LOGI(TAG, "%s", rx_buffer.c_str());
                 if (rx_buffer.find("CAMERA_RECORD_START") != std::string::npos) {
                     gctx.logger_control.active = true;
@@ -102,12 +108,39 @@ void firefly_x_lite_task(void* param) {
                 }
             }
         }
-
         if (sock != -1) {
             ESP_LOGE(TAG, "Shutting down socket and restarting...");
             shutdown(sock, 0);
             close(sock);
         }
+    }
+}
+
+void gopro_wifi_task(void* param) {
+    bool prev_active = false;
+
+    int err{}, sock{};
+    while (1) {
+        if (prev_active != gctx.logger_control.active) {
+            prev_active = gctx.logger_control.active;
+            if (!make_tcp_conn("10.5.5.9", 80, &sock)) {
+                continue;
+            }
+            auto record_start = "GET /gp/gpControl/command/shutter?p=1 HTTP/1.1\n\n";
+            auto record_stop = "GET /gp/gpControl/command/shutter?p=0 HTTP/1.1\n\n";
+            if (gctx.logger_control.active) {
+                err = send(sock, record_start, strlen(record_start), 0);
+            } else {
+                err = send(sock, record_stop, strlen(record_stop), 0);
+            }
+            if (err < 0) {
+                ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
+            }
+            // vTaskDelay(2000 / portTICK_PERIOD_MS);
+            shutdown(sock, 0);
+            close(sock);
+        }
+        vTaskDelay(50 / portTICK_PERIOD_MS);
     }
 }
 
@@ -197,6 +230,9 @@ void cam_control_task(void* param) {
         } break;
         case 3: {
             runcam_protocol_listen_task(param);
-        }
+        } break;
+        case 4: {
+            gopro_wifi_task(param);
+        } break;
     }
 }
