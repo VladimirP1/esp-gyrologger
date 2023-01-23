d646c94737e5a44b75eaca8b8bb2c48f943bd05c
Vladimir Pinchuk <vladimir.pinchuk01@gmail.com>
2023-01-23 23:10:55 +0300

    change probe order

--- main/gyro/gyro_probe.cpp
+++ main/gyro/gyro_probe.cpp
@@ -21,7 +21,7 @@ void gyro_probe_and_start_task() {
     static const std::initializer_list<probe_entry> probe_list = {
         {0x68, probe_bmi160, gyro_bmi160_task},     {0x69, probe_bmi160, gyro_bmi160_task},
         {0x6a, probe_lsm6, gyro_lsm6_task},         {0x6b, probe_lsm6, gyro_lsm6_task},
-        {0x68, probe_mpu6050, gyro_mpu6050_task},   {0x69, probe_mpu6050, gyro_mpu6050_task},
+        {0x69, probe_mpu6050, gyro_mpu6050_task},   {0x68, probe_mpu6050, gyro_mpu6050_task},
         {0x68, probe_icm42688, gyro_icm42688_task}, {0x69, probe_icm42688, gyro_icm42688_task}};
 
     for (auto& entry : probe_list) {
