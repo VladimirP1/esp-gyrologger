7ae5020f767cc309a7a09f6c834cb1a246d922e8
Vladimir Pinchuk <vladimir.pinchuk01@gmail.com>
2023-05-18 00:55:39 +0300

    fix mpu6886

--- main/gyro/gyro_mpu6050.cpp
+++ main/gyro/gyro_mpu6050.cpp
@@ -66,32 +66,45 @@ static const char *TAG = "gyro_mpu";
 
 #define REG_WHO_AM_I 0x75
 
-#define FIFO_SAMPLE_SIZE 12
+enum {IMU_MPU6000, IMU_MPU6886};
+static int fifo_sample_size{};
+static int mpu_imu_type{};
 
 static void IRAM_ATTR gyro_i2c_fifo_bytes_cb(void *arg);
 
-static void IRAM_ATTR gyro_i2c_cb(void *arg) {
+static void IRAM_ATTR gyro_i2c_cb_mpu(void *arg) {
     if (gctx.terminate_for_update) {
         return;
     }
 
-    static uint8_t tmp_data[FIFO_SAMPLE_SIZE];
+    static uint8_t tmp_data[14];
     static uint64_t prev_time = 0;
     uint64_t time = esp_timer_get_time();
-    if (mini_i2c_read_reg_get_result(tmp_data, FIFO_SAMPLE_SIZE) == ESP_OK) {
-        gctx.gyro_ring->Push((time - prev_time) * 1000,
-                             static_cast<int>((int16_t)((tmp_data[6] << 8) | tmp_data[7])),
-                             static_cast<int>((int16_t)((tmp_data[8] << 8) | tmp_data[9])),
-                             static_cast<int>((int16_t)((tmp_data[10] << 8) | tmp_data[11])),
-                             static_cast<int>((int16_t)((tmp_data[0] << 8) | tmp_data[1])),
-                             static_cast<int>((int16_t)((tmp_data[2] << 8) | tmp_data[3])),
-                             static_cast<int>((int16_t)((tmp_data[4] << 8) | tmp_data[5])),
-                             kFlagHaveAccel);
+    if (mini_i2c_read_reg_get_result(tmp_data, fifo_sample_size) == ESP_OK) {
+        int16_t gyr[3];
+        int16_t acc[3];
+        if (mpu_imu_type == IMU_MPU6000) {
+            gyr[0] = (int16_t)((tmp_data[6] << 8) | tmp_data[7]);
+            gyr[1] = (int16_t)((tmp_data[8] << 8) | tmp_data[9]);
+            gyr[2] = (int16_t)((tmp_data[10] << 8) | tmp_data[11]);
+            acc[0] = (int16_t)((tmp_data[0] << 8) | tmp_data[1]);
+            acc[1] = (int16_t)((tmp_data[2] << 8) | tmp_data[3]);
+            acc[2] = (int16_t)((tmp_data[4] << 8) | tmp_data[5]);
+        } else if (mpu_imu_type == IMU_MPU6886) {
+            gyr[0] = (int16_t)((tmp_data[8] << 8) | tmp_data[9]);
+            gyr[1] = (int16_t)((tmp_data[10] << 8) | tmp_data[11]);
+            gyr[2] = (int16_t)((tmp_data[12] << 8) | tmp_data[13]);
+            acc[0] = (int16_t)((tmp_data[0] << 8) | tmp_data[1]);
+            acc[1] = (int16_t)((tmp_data[2] << 8) | tmp_data[3]);
+            acc[2] = (int16_t)((tmp_data[4] << 8) | tmp_data[5]);
+        }
+        gctx.gyro_ring->Push((time - prev_time) * 1000, gyr[0], gyr[1], gyr[2], acc[0], acc[1],
+                             acc[2], kFlagHaveAccel);
         prev_time = time;
     } else {
         mini_i2c_hw_fsm_reset();
     }
-    
+
     proc_aux_i2c();
     mini_i2c_read_reg_callback(gctx.gyro_i2c_adr, REG_FIFO_COUNT_H, 2, gyro_i2c_fifo_bytes_cb,
                                nullptr);
@@ -115,9 +128,9 @@ static void IRAM_ATTR gyro_i2c_fifo_bytes_cb(void *arg) {
         mini_i2c_hw_fsm_reset();
     }
 
-    if (fifo_bytes > 0) {
-        mini_i2c_read_reg_callback(gctx.gyro_i2c_adr, REG_FIFO_RW, FIFO_SAMPLE_SIZE, gyro_i2c_cb,
-                                   NULL);
+    if (fifo_bytes >= fifo_sample_size) {
+        mini_i2c_read_reg_callback(gctx.gyro_i2c_adr, REG_FIFO_RW, fifo_sample_size,
+                                   gyro_i2c_cb_mpu, NULL);
     } else {
         mini_i2c_read_reg_callback(gctx.gyro_i2c_adr, REG_FIFO_COUNT_H, 2, gyro_i2c_fifo_bytes_cb,
                                    nullptr);
@@ -133,15 +146,32 @@ bool probe_mpu6050(uint8_t dev_adr) {
 }
 
 void gyro_mpu6050_task(void *params_pvoid) {
-    gctx.gyro_sr = 2000.0;
-    gctx.accel_sr = 2000.0;
-
     /* This is needed for 2k sample rate */
     mini_i2c_set_timing(500000);
 
     /* Mpu6050 needs larger delays between transfers */
     mini_i2c_double_stop_timing();
     mini_i2c_double_stop_timing();
+
+    /* Detect imu type */
+    uint8_t data[1];
+    mini_i2c_read_reg_sync(gctx.gyro_i2c_adr, REG_WHO_AM_I, data, 1);
+    if (data[0] == 0x68) {
+        ESP_LOGI(TAG, "IMU is MPU6000");
+        mpu_imu_type = IMU_MPU6000;
+        fifo_sample_size = 12;
+        gctx.gyro_sr = 2000.0;
+        gctx.accel_sr = 2000.0;
+    } else if (data[0] == 0x19) {
+        ESP_LOGI(TAG, "IMU is MPU6886");
+        mpu_imu_type = IMU_MPU6886;
+        fifo_sample_size = 14;
+        gctx.gyro_sr = 1000.0;
+        gctx.accel_sr = 1000.0;
+    } else {
+        ESP_LOGE(TAG, "bad whoami");
+        vTaskDelete(NULL);
+    }
 
     /* Reset */
     ESP_ERROR_CHECK(
@@ -157,9 +187,16 @@ void gyro_mpu6050_task(void *params_pvoid) {
                                 REG_PWR_MGMT_1_VALUE_CLKSEL_ZGYRO << REG_PWR_MGMT_1_BIT_CLKSEL_0));
     ESP_LOGI(TAG, "IMU change clock");
 
-    ESP_ERROR_CHECK(
-        mini_i2c_write_reg_sync(gctx.gyro_i2c_adr, REG_CONFIG, 0 << REG_CONFIG_BIT_DLPF_CFG_0));
-    ESP_ERROR_CHECK(mini_i2c_write_reg_sync(gctx.gyro_i2c_adr, REG_SMPRT_DIV, 3));
+    if (mpu_imu_type == IMU_MPU6000) {
+        ESP_ERROR_CHECK(
+            mini_i2c_write_reg_sync(gctx.gyro_i2c_adr, REG_CONFIG, 0 << REG_CONFIG_BIT_DLPF_CFG_0));
+        ESP_ERROR_CHECK(mini_i2c_write_reg_sync(gctx.gyro_i2c_adr, REG_SMPRT_DIV, 3));
+    } else if (mpu_imu_type == IMU_MPU6886) {
+        ESP_ERROR_CHECK(
+            mini_i2c_write_reg_sync(gctx.gyro_i2c_adr, REG_CONFIG, 1 << REG_CONFIG_BIT_DLPF_CFG_0));
+        ESP_ERROR_CHECK(mini_i2c_write_reg_sync(gctx.gyro_i2c_adr, REG_SMPRT_DIV, 0));
+    }
+
     ESP_ERROR_CHECK(
         mini_i2c_write_reg_sync(gctx.gyro_i2c_adr, REG_GYRO_CONFIG,
                                 REG_GYRO_CONFIG_VALUE_FS_1000_DPS << REG_GYRO_CONFIG_BIT_FS_SEL_0));
